004901980939070ff3858e9d1ea7f14727e093cf
Eric Kirima <ekirima@gmail.com>
2018-12-08 13:27:32 +0300

    Intitialised Ultimate blog repo

---
+++ index.php
@@ -0,0 +1,73 @@
+<html>
+<title>Ultimate Blog</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+<head></head>
+<body>
+<div class="jumbotron">
+  <h1 class="display-4">Ultimate Blog</h1>
+  <p class="lead">My Blog</p>
+  <hr class="my-4">
+  <p>Get latest News</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+<div class="container">
+<div class="row">
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>The title of article</h3>
+            </div>
+            <div class="card-body">
+            <img src="images/y.jpg" class="img-fluid" alt="Image">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis optio totam fuga? Doloribus, dolorum numquam, repellendus aut laboriosam odio cupiditate voluptatibus,
+             at dignissimos maxime modi optio accusamus
+              sed doloremque?</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-outline-success">Read More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>The title of article</h3>
+            </div>
+            <div class="card-body">
+            <img src="images/y.jpg" class="img-fluid" alt="Image">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis optio totam fuga? Doloribus, dolorum numquam, repellendus aut laboriosam odio cupiditate voluptatibus,
+             at dignissimos maxime modi optio accusamus
+              sed doloremque?</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-outline-success">Read More</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>The title of article</h3>
+            </div>
+            <div class="card-body">
+            <img src="images/y.jpg" class="img-fluid" alt="Image">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis optio totam fuga? Doloribus, dolorum numquam, repellendus aut laboriosam odio cupiditate voluptatibus,
+             at dignissimos maxime modi optio accusamus
+              sed doloremque?</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-outline-success">Read More</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<footer class="bg-primary text-center">
+<p>All Rights Reserved &copy; Ultimate Blog</p>
+</footer>
+</body>
+</html>
